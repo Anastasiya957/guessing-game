ccd4a0733bd26f434c8315abbbf00c80f4b909de
Anastasiya957 <anastasiyabarysheva95@gmail.com>
2017-09-29 18:54:15 +0300

    implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	var L = 0;
+    	var R = 0;
+    	var m = 0;
+    }
 
     setRange(min, max) {
-
+    	this.L=min;
+    	this.R=max;
     }
 
     guess() {
-
+    	this.m = Math.ceil( (this.L+this.R)/2 );
+    	return this.m;
     }
 
     lower() {
-
+    	this.R = this.m;
     }
 
     greater() {
-
+    	this.L = this.m;
     }
 }
 
